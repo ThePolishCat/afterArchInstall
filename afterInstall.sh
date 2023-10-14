58537c138328599df300026140a3371fbc0c2eb3
ThePolishCat <damianadam000@gmail.com>
2023-10-14 23:13:38 +0200

    zmieniono:       afterInstall.sh 	zmieniono:       etc/pacman.conf

--- afterInstall.sh
+++ afterInstall.sh
@@ -6,7 +6,7 @@ sudo pacman-key --recv-keys 66576FE3626A02F272FF2933330E1ADB4E5E62AA
 sudo pacman-key --lsign-key 66576FE3626A02F272FF2933330E1ADB4E5E62AA
 sudo cp etc/pacman.conf /
 sudo pacman -Syy
-sudo pacman -S  --noconfirm zsh yay nerd-fonts
+sudo pacman -S  --noconfirm zsh yay nerd-fonts lsd bat tldr	
 chsh -s /bin/zsh
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 cp .zshrc $HOME/
