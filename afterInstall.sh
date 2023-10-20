c0c2892c999ac5f43bf20bd5c6e06583ab722706
ThePolishCat <damianadam000@gmail.com>
2023-10-20 14:56:57 +0200

    zmieniono:       afterInstall.sh 	zmieniono:       etc/pacman.conf

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-cp .local $HOME/
+cp -r .local $HOME/
 cp .p10k.zsh $HOME/
 sudo pacman-key --init
 sudo pacman-key --recv-keys 66576FE3626A02F272FF2933330E1ADB4E5E62AA
 sudo pacman-key --lsign-key 66576FE3626A02F272FF2933330E1ADB4E5E62AA
-sudo cp etc/pacman.conf /
+sudo cp -r etc /
 sudo pacman -Syy
 sudo pacman -S  --noconfirm zsh yay nerd-fonts lsd bat tldr	
 chsh -s /bin/zsh
+rm $HOME/.zshrc
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 cp .zshrc $HOME/
-echo zmodule romkatv/powerlevel10k --use degit >> .zimrc
+echo zmodule romkatv/powerlevel10k --use degit >> $HOME/.zimrc
 zimfw install
 
